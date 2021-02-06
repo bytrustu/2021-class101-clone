36e5a7396929d800c30ecb61867d5f5bc20faf48
bytrustu <bytrustu@gmail.com>
2021-02-06 18:40:25 +0900

    [FIX] useCounter action 함수가 조건에 맞지 않게 실행되는 이슈 수정

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -4,12 +4,16 @@ import { IUseCounter } from '../types'
 const useCounter: IUseCounter = (currentNumber, maxNumber) => {
   const [count, setCount] = useState<number>(currentNumber)
   const countUp = (action: () => any) => {
-    if (count < maxNumber) setCount((prev) => prev + 1)
-    action && action()
+    if (count < maxNumber) {
+      setCount((prev) => prev + 1)
+      action && action()
+    }
   }
   const countDown = (action: () => any) => {
-    if (count > 1) setCount((prev) => prev - 1)
-    action && action()
+    if (count > 1) {
+      setCount((prev) => prev - 1)
+      action && action()
+    }
   }
   return { count, countUp, countDown }
 }
